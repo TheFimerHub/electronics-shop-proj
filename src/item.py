c85d624f9dc5f4f7c4b65ffb04fed9a61c8e975e
TheFimerHub <m.stepanov@it-park.tech>
2023-09-28 16:46:42 +0300

    homework_2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -7,13 +9,13 @@ class Item:
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
-        Создание экземпляра класса item.
+        Создание экземпляра класса Item.
 
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
@@ -31,3 +33,30 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) > 10:
+            name = name[:10]
+        self.__name = name
+
+    @classmethod
+    def instantiate_from_csv(cls, path):
+        """
+        Создает экземпляры товаров из CSV-файла и добавляет их в список.
+
+        Args:
+            path (str): Путь к CSV-файлу.
+        """
+        with open(path, newline='', encoding='cp1251') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                cls(row['name'], row['price'], row['quantity'])
+
+    @staticmethod
+    def string_to_number(txt):
+        return int(float(txt))
--- tests/test_item.py
+++ tests/test_item.py
@@ -22,12 +22,24 @@ def test_apply_discount():
     assert item1.price == 8000.0
     assert item2.price == 20000
 
-def test_all_items():
-    item1 = Item("Смартфон", 10000, 20)
-    item2 = Item("Ноутбук", 20000, 5)
 
-    assert item1 in Item.all
-    assert item2 in Item.all
+def test_string_to_number():
+    """
+    Тест проверяет, что метод string_to_number корректно преобразует строку в число.
+    """
+    assert Item.string_to_number('10.5') == 10
+    assert Item.string_to_number('20.7') == 20
+    assert Item.string_to_number('5.0') == 5
+
+
+def test_instantiate_from_csv():
+    """
+    Тест проверяет, что метод instantiate_from_csv корректно создает экземпляры товаров из CSV-файла.
+    """
+    Item.instantiate_from_csv("./src/items.csv")
+    assert len(Item.all) == 9
+
+
 
 if __name__ == "__main__":
     pytest.main()
